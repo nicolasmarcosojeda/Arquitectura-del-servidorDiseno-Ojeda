17512ac4120d9934b4ddfa356db4666496c8d1e8
nicolasmarcosojeda <marcosnicolass.74@gmail.com>
2024-03-25 22:49:31 -0300

    Documentacion de API

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -28,12 +28,41 @@ const productsController = {
       res.status(500).json({ error: 'Error al obtener los productos' });
     }
   },
+  /**
+   * Obtiene un producto por su ID.
+   * @memberof productsController
+   * @function getProductById
+   * @param {Object} req - Objeto de solicitud.
+   * @param {Object} res - Objeto de respuesta.
+   * @returns {Object} - Retorna el producto encontrado por su ID.
+   * @throws {Error} - Retorna un error si falla la obtención del producto.
+   */
   getProductById: async (req, res) => {
     // Implementar la lógica para obtener un producto por su ID
   },
+
+  /**
+   * Agrega un nuevo producto.
+   * @memberof productsController
+   * @function addProduct
+   * @param {Object} req - Objeto de solicitud.
+   * @param {Object} res - Objeto de respuesta.
+   * @returns {Object} - Retorna el producto recién agregado.
+   * @throws {Error} - Retorna un error si falla la adición del producto.
+   */
   addProduct: async (req, res) => {
     // Implementar la lógica para agregar un nuevo producto
   },
+
+  /**
+   * Modifica un producto existente.
+   * @memberof productsController
+   * @function modifyProduct
+   * @param {Object} req - Objeto de solicitud.
+   * @param {Object} res - Objeto de respuesta.
+   * @returns {Object} - Retorna el producto modificado.
+   * @throws {Error} - Retorna un error si falla la modificación del producto.
+   */
   modifyProduct: async (req, res) => {
     const productId = req.params.id;
     const user = req.user; // Suponiendo que tienes el usuario autenticado en req.user
@@ -44,6 +73,16 @@ const productsController = {
 
     // Implementar la lógica para modificar el producto
   },
+
+  /**
+   * Elimina un producto existente.
+   * @memberof productsController
+   * @function deleteProduct
+   * @param {Object} req - Objeto de solicitud.
+   * @param {Object} res - Objeto de respuesta.
+   * @returns {Object} - Retorna un mensaje de éxito indicando que el producto ha sido eliminado.
+   * @throws {Error} - Retorna un error si falla la eliminación del producto.
+   */
   deleteProduct: async (req, res) => {
     const productId = req.params.id;
     const user = req.user; // Suponiendo que tienes el usuario autenticado en req.user
@@ -54,7 +93,6 @@ const productsController = {
 
     // Implementar la lógica para eliminar el producto
   },
-  // Agrega más controladores según sea necesario
 };
 
 export default productsController;
